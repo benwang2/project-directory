a549c7df63b5d8c0282d8632a55b32b8f9e9b5fb
Benjamin Wang <eb.benwang@gmail.com>
2022-04-18 15:41:34 -0400

    Updates

---
+++ precache-manifest.11d7766abace54b00513518edfbe47e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "319881a19e9cc4c9d707",
+    "url": "/project-directory/static/css/main.a916894e.chunk.css"
+  },
+  {
+    "revision": "319881a19e9cc4c9d707",
+    "url": "/project-directory/static/js/main.319881a1.chunk.js"
+  },
+  {
+    "revision": "d86ecc4416c870e57713",
+    "url": "/project-directory/static/js/1.d86ecc44.chunk.js"
+  },
+  {
+    "revision": "ad94ade114d28abdbb03",
+    "url": "/project-directory/static/js/runtime~main.ad94ade1.js"
+  },
+  {
+    "revision": "d3e2cbad3d2413469249507b1e7457e1",
+    "url": "/project-directory/index.html"
+  }
+];
